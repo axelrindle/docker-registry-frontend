3f12faa53ddcd35e090aaa6e99eacd35be56119e
Axel Rindle <mail@axelrindle.de>
2022-03-23 15:32:12 +0100

    Leverage buildx caching

--- docker/build.sh
+++ docker/build.sh
@@ -5,6 +5,8 @@ TAG="${TAG:-axelrindle/docker-registry-frontend:latest}"
 echo "Building as $TAG"
 
 docker buildx build \
+    --cache-from=type=local,src=.buildx-cache \
+    --cache-to=type=local,dest=.buildx-cache \
     --push \
     --platform linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/amd64 \
     -t $TAG \
