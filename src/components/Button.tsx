8994d27cb092e6b0710b5f904594eb52fee52af4
Axel Rindle <mail@axelrindle.de>
2022-04-23 16:36:20 +0200

    Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+import { LegacyRef, MouseEventHandler, ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export interface Props {
+    children?: ReactNode
+    className?: string
+    onClick?: MouseEventHandler
+    ref?: LegacyRef<any>
+}
+
+const defaultClassName = `
+flex gap-2 items-center justify-center
+py-2 px-4 mb-2
+shadow rounded
+cursor-pointer
+bg-docker text-white
+transition-colors hover:bg-blue-500
+`
+
+export default function Button(props: Props) {
+    const className = twMerge(defaultClassName, props.className)
+
+    return (
+        <div
+            className={className}
+            onClick={props.onClick}
+            ref={props.ref}
+        >
+            {props.children}
+        </div>
+    )
+}
